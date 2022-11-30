436a1595274b48269f1a55783591076201f81544
yilin_huang <942519438@qq.com>
2022-11-30 10:42:52 +0800

    headers

--- user/uthread.c
+++ user/uthread.c
@@ -1,10 +1,10 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
+#include "kernel/riscv.h"
+#include "kernel/spinlock.h"
 #include "kernel/param.h"
 #include "user/user.h"
 #include "kernel/proc.h"
-#include "kernel/riscv.h"
-#include "kernel/spinlock.h"
 
 
 /* Possible states of a thread: */
