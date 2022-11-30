9b486498a7d467328fddea485f7dfb4be4953025
yilin_huang <942519438@qq.com>
2022-11-30 11:33:58 +0800

    ph

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -7,6 +7,7 @@
 
 #define NBUCKET 5
 #define NKEYS 100000
+pthread_mutex_t phlocks[NBUCKET];
 
 struct entry {
   int key;
@@ -40,7 +41,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&phlocks[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -54,7 +55,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&phlocks[i]);
 }
 
 static struct entry*
@@ -118,6 +119,9 @@ main(int argc, char *argv[])
     keys[i] = random();
   }
 
+  for (int i = 0; i < NBUCKET; ++i){
+    pthread_mutex_init(&phlocks[i], NULL);
+  }
   //
   // first the puts
   //
